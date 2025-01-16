a9b39f608814cc7154b8dd01dd0000d6b32874f4
Silvia Tittarelli <s11082906@liceodavincijesi.edu.it>
2025-01-16 09:53:10 +0100

    veicolo completato

--- veicolo.py
+++ veicolo.py
@@ -7,8 +7,8 @@ marcheMoto = ["suzuki", "honda", "yamaha", "ktm", "vespa", "beta"]
 marcheAuto = ["jeep", "toyota", "lamborghini", "lancia", "tesla", "ford", "fiat", "suzuki", "alfa romeo"]
 coloriAccettabili = ["nero", "bianco", "giallo", "blu", "rosso", "viola", "rosa", "verde"]
 alimentazioneAccettabili = ["GPL", "benzina", "diesel", "ibride"]
-alfabetoMaiuscolo = ["QWERTYUIOPASDFGHJKLZXCVBNM"]
-cifre = ["1234567890"]
+alfabetoMaiuscolo = "QWERTYUIOPASDFGHJKLZXCVBNM"
+cifre = "1234567890"
 listaMarche = []
 listaModelli = []
 listaCilindrate = []
@@ -19,53 +19,78 @@ class Veicolo:
         """
         inizializza la funzione
         """
-        #la targa deve essere della forma AB 123 CD (dove al posto delle ABCD ci va una qualunque lettera maiuscola dell’alfabeto e al posto di 123 ci va una qualunque sequenza numerica di 3 cifre).
-        if targa[0] and targa[1] and targa[7] and targa[8] in alfabetoMaiuscolo and targa[3] and targa[4] and targa[5] in cifre:
-            targa = self.__targa
-        else:
-            raise ValueError("targa non valida!")
-        
-        if marca in marcheMoto or marca in marcheAuto:
-            marca = self.__marca
-            
+        self.__marca = ""
         self.__modello = ""
+        self.__colore = ""
+        self.__cilindrata = 0
+        self.__alimentazione = ""
         
-        if colore in coloriAccettabili:
-            colore = self.__colore
-              
-        #la cilindrata deve essere un intero positivo multiplo di 100. 
-        if cilindrata % 100 == 0:
-            self.__cilindrata = cilindrata
+        #la targa deve essere della forma AB 123 CD (dove al posto delle ABCD ci va una qualunque lettera maiuscola dell’alfabeto e al posto di 123 ci va una qualunque sequenza numerica di 3 cifre).
+        if len(targa) != 7:
+            raise ValueError("£Che cavolo di targa hai messo?")
+        if targa[0] in alfabetoMaiuscolo and targa[1] in alfabetoMaiuscolo and targa[5] in alfabetoMaiuscolo and targa[6] in alfabetoMaiuscolo and targa[2] in cifre and targa[3] in cifre and targa[4] in cifre:
+            self.__targa = targa
         else:
-            raise ValueError("cilindrata non valida")
-        
-        if alimentazione in alimentazioneAccettabili:
-            alimentazione = self.__alimentazione
+            raise ValueError("Targa fatta male")
         
     @property
     def targa(self):
-        return self.__targa 
+        return self.__targa
     
     @property
     def marca(self):
         return self.__marca
     
+    @marca.setter
+    def marca(self, marca):
+        if marca not in marcheAuto or marca not in marcheMoto:
+            raise ValueError("marca non valida")
+        self.__marca = marca
+        return
+    
     @property
     def modello(self):
         return self.__modello
+    
+    @modello.setter
+    def modello(self, modello):
+        if modello not in listaModelli:
+            raise ValueError("modello non valido")
+        self.__modello = modello
+        return
     
     @property
     def colore(self):
         return self.__colore
     
+    @colore.setter
+    def colore(self, colore):
+        if colore not in coloriAccettabili:
+            raise ValueError("colore non valido")
+        self.__colore = colore
+        return
+        
     @property
     def cilindrata(self):
         return self.__cilindrata
     
+    @cilindrata.setter
+    def cilindrata(self, cilindrata):
+        if cilindrata % 100 != 0:
+            raise ValueError("cilindrata non valida")
+        self.__cilindrata = cilindrata
+        return
+            
     @property
     def alimentazione(self):
         return self.__alimentazione
     
+    @alimentazione.setter
+    def alimentazione(self, alimentazione):
+        if alimentazione not in alimentazioneAccettabili:
+            raise ValueError("alimentazione non valida")
+        self.__alimentazione = alimentazione
+        return
     #ritorna una stringa  con le variabili
     def __str__(self):
         return __class__.__name__ + str(self.__dict__)
@@ -76,36 +101,24 @@ class Veicolo:
     
 #Aggiungo un ordinamento implicito fra gli oggetti di tipo Veicolo
 #in modo da renderli ordinabili alfabeticamente per marca, modello e numericamente (dal più piccolo al più grande) per cilindrata
-    def ordinamentoMarca(self):
-        listaMarche.append(self.__marca)
-        listaMarche.sort()
-        return
-        
-    def ordinamentoModello(self):
-        listaModelli.append(self.__modello)
-        listaModelli.sort()
-        return
-        
-    def ordinamentoCilindrata(self):
-        listaCilindrate.append(self.__cilindrata)
-        listaCilindrate.sort()
-        return
-    
-    #controllo cilindrata
+ 
     def __lt__(self, other):
-        if self.__cilindrata < other.__cilindrata:
+        if self.__marca < other.__marca:
             return True
-        else:
-            return False
-            
+        elif self.__marca == other.__marca:
+            if self.__modello < other.__modello:
+                return True
+        elif self.__modello == other.__modello:
+            if self.__cilindrata < other.__cilindrata:
+                return True       
            
 #----------------------------------------------------------------------------
 if __name__ == "__main__":
-    veicolo1 = Veicolo("AB 123 CD")
+    veicolo1 = Veicolo("AB123CD")
     print(veicolo1)
     
-    veicolo2 = Veicolo("C4 123 6D")
-    print(veicolo2)
+    #veicolo2 = Veicolo("C41236D")
+    #print(veicolo2)
     
 
     
